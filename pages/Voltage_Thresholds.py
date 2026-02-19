c888a07a0c9407d3aaf019dab461f03f16b63e0e
Jaideep-design <jaideep.gupta@ecozensolutions.com>
2026-02-19 11:46:08 +0530

    Update Voltage_Thresholds.py

--- pages/Voltage_Thresholds.py
+++ pages/Voltage_Thresholds.py
@@ -372,7 +372,7 @@ if st.session_state.state == "WRITE_VALUE" and st.session_state.write_unlocked:
 
     value = st.number_input(
         "Voltage (V)",
-        min_value=150,
+        min_value=130,
         max_value=300,
         value=st.session_state.write_value or 230
     )
